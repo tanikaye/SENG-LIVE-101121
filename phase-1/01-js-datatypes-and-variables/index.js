e0c971a3f467f44729a8d44da55e1940680ad911
Tani Kaye <1tanikaye@gmail.com>
2021-10-12 15:40:58 -0400

    completed all three breakout assignments

--- phase-1/01-js-datatypes-and-variables/index.js
+++ phase-1/01-js-datatypes-and-variables/index.js
@@ -1,188 +1,207 @@
-// ❗ Use console.log() To Check Answers
+// // ❗ Use console.log() To Check Answers
 
-// ✅ Using console.log()
+// // ✅ Using console.log()
 
-    // console.log("Hey there!");
+//     // console.log("Hey there!");
 
-    // let x = 2;
+//     // let x = 2;
 
-    // let y = 2;
+//     // let y = 2;
 
-    // console.log(x + y);
+//     // console.log(x + y);
 
-// ✅ JS Data Types (Primitive)
+// // ✅ JS Data Types (Primitive)
 
-    // String
+//     // String
 
-        // console.log(typeof "here");
+//         // console.log(typeof "here");
 
-    // Number
+//     // Number
 
-        // console.log(typeof 5);
+//         // console.log(typeof 5);
 
-    // Boolean
+//     // Boolean
 
-        // console.log(typeof true);
+//         // console.log(typeof true);
 
-    // Undefined => empty value
+//     // Undefined => empty value
 
-        // console.log(typeof undefined);
+//         // console.log(typeof undefined);
 
-    // Null => absence of value
+//     // Null => absence of value
 
-        // console.log(typeof null);
+//         // console.log(typeof null);
 
-    // Symbol => unique identifier
+//     // Symbol => unique identifier
 
-        // let id = Symbol("test");
+//         // let id = Symbol("test");
 
-        // console.log(typeof id);
-    
-    // BigInt => numbers larger than those JS can represent with a Number
+//         // console.log(typeof id);
 
-        // let bigNum = BigInt(123456789101112131415);
+//     // BigInt => numbers larger than those JS can represent with a Number
 
-        // console.log(typeof bigNum);
+//         // let bigNum = BigInt(123456789101112131415);
 
-// ✅ let vs. const
+//         // console.log(typeof bigNum);
 
-    // 💡 what are main differences / use cases?
+// // ✅ let vs. const
 
-    // let
+//     // 💡 what are main differences / use cases?
 
-        // let x = 10;
+//     // let
 
-        // let x = 5;
+//         // let x = 10;
 
-    // const
+//         // let x = 5;
 
-        // const x = 10;
+//     // const
 
-        // const x;
-        
-        // x = 5;
+//         // const x = 10;
 
-// ✅ Conditional Statements
+//         // const x;
 
-    // if...else
+//         // x = 5;
 
-        // let x = 5;
+// // ✅ Conditional Statements
 
-        // if (x === 5) {
-        //     console.log("Equal to 5!");
-        // } else {
-        //     console.log("Not equal to 5!");
-        // }
+//     // if...else
 
-    // if...else if...else
+//         // let x = 5;
 
-        // let x = 5;
+//         // if (x === 5) {
+//         //     console.log("Equal to 5!");
+//         // } else {
+//         //     console.log("Not equal to 5!");
+//         // }
 
-        // if (x > 5) {
-        //     console.log("Greater than five!");
-        // } else if (x === 5) {
-        //     console.log("Equal to five!");
-        // } else {
-        //     console.log("Less than five!");
-        // }
+//     // if...else if...else
 
-    // ternary operator
+//         // let x = 5;
 
-        // let x = 5;
+//         // if (x > 5) {
+//         //     console.log("Greater than five!");
+//         // } else if (x === 5) {
+//         //     console.log("Equal to five!");
+//         // } else {
+//         //     console.log("Less than five!");
+//         // }
 
-        // { x === 5 ? console.log("Equal to five!") : console.log("Not equal to five!") }
+//     // ternary operator
 
-console.log("------------------------");
-console.log("⬇️ Break Out Activites ⬇️");
-console.log("🚨 Comment Out Lecture Code Above Before Starting 🚨");
-console.log("💡 Use console.log() To Check Answers 💡");
-console.log("------------------------");
+//         // let x = 5;
 
-// 🚧 Break Out Activity 1: Using let vs. const
+//         // { x === 5 ? console.log("Equal to five!") : console.log("Not equal to five!") }
 
-// Uncomment the code below to properly declare and assign a value to "x" using "const"
+// console.log("------------------------");
+// console.log("⬇️ Break Out Activites ⬇️");
+// console.log("🚨 Comment Out Lecture Code Above Before Starting 🚨");
+// console.log("💡 Use console.log() To Check Answers 💡");
+// console.log("------------------------");
 
-    // const x;
+// // 🚧 Break Out Activity 1: Using let vs. const
+
+// // Uncomment the code below to properly declare and assign a value to "x" using "const"
+
+//     // const x;
 
     // const x = 5;
 
     // const x;
 
     // console.log(`Success! x = ${x}`);
-    
-    // 🚨 Be sure to comment out any code above before proceeding to the next activity.
 
-// Uncomment the code below to properly declare and assign a value to "y" using "let"
+//     // 🚨 Be sure to comment out any code above before proceeding to the next activity.
 
-    // let y;
+// // Uncomment the code below to properly declare and assign a value to "y" using "let"
 
-    // let y = 5; 
+//     // let y;
 
-    // let y;
+//     // let y = 5;
 
-    // console.log(`Success! y = ${y}`);
+//     // let y;
 
-    // 🚨 Be sure to comment out any code above before proceeding to the next activity.
+//     // console.log(`Success! y = ${y}`);
 
-// Uncomment the code below to get a successful response in console
+//     // 🚨 Be sure to comment out any code above before proceeding to the next activity.
 
-    // const a = 2;
-    
-    // a = 2;
-    
-    // let b;
-    
-    // let b = 4
-    
-    // console.log(`Success! ${a} + ${a} = ${b}`);
+// // Uncomment the code below to get a successful response in console
 
-    // 🚨 Be sure to comment out any code above before proceeding to the next activity.
+//     const a = 2;
 
-// 🚧 Break Out Activity 2: Using if...else
+// //     // a = 2;
 
-    // 🚧 Set a value for "time" between 0 and 24
-    // let time;
+// //     // let b;
 
-    // If "time" is less than or equal to 12...
-    // if (❓) {
+//     let b = 4
 
-        // console.log(`${time} is in the AM.`)
+//     console.log(`Success! ${a} + ${a} = ${b}`);
 
-    // In all other cases...
-    // } else {
+//     // 🚨 Be sure to comment out any code above before proceeding to the next activity.
 
-        // console.log(`${time} is in the PM.`)
+// // 🚧 Break Out Activity 2: Using if...else
 
-    // }
+//     // 🚧 Set a value for "time" between 0 and 24
+    let time = 12;
 
-    // ✨ BONUS: Try refactoring the above expression using a ternary operator.
+//     // If "time" is less than or equal to 12...
+//     if (time <= 12) {
 
-    // 🚨 Be sure to comment out any code above before proceeding to the next activity.
+//         console.log(`${time} is in the AM.`)
 
-// 🚧 Break Out Activity 3: Using if...else...if
+// //     // In all other cases...
+//     } else {
 
-    // We are given a variable "marks." Our task is to print:
+//         console.log(`${time} is in the PM.`)
 
-    // - "AA" if marks is greater than 90.
-    // - "AB" if marks is greater than 80 and less than or equal to 90.
-    // - "BB" if marks is greater than 70 and less than or equal to 80.
-    // - "BC" if marks is greater than 60 and less than or equal to 70.
-    // - "CC" if marks is greater than 50 and less than or equal to 60.
-    // - "CD" if marks is greater than 40 and less than or equal to 50.
-    // - "DD" if marks is greater than 30 and less than or equal to 40.
-    // - "FF" if marks is less than or equal to 30.
+//     }
 
-    // 🚨 Assign a value to "marks" between 0 and 100
-    
-        // let marks;
+{
+    time <= 12 ? console.log(`${time} is in the AM`) : console.log(`${time} is in the PM`)
+}
 
-    // 🚨 Format your if...else if...else conditional below
-    
-        // if (❓) {
-        // ... 
-        // } else if (❓) {
-        // ...
-        // ...
-        // } else {
-        // ...
-        // }
+//     // ✨ BONUS: Try refactoring the above expression using a ternary operator.
+
+//     // 🚨 Be sure to comment out any code above before proceeding to the next activity.
+
+// // 🚧 Break Out Activity 3: Using if...else...if
+
+//     // We are given a variable "marks." Our task is to print:
+
+//     // - "AA" if marks is greater than 90.
+//     // - "AB" if marks is greater than 80 and less than or equal to 90.
+//     // - "BB" if marks is greater than 70 and less than or equal to 80.
+//     // - "BC" if marks is greater than 60 and less than or equal to 70.
+//     // - "CC" if marks is greater than 50 and less than or equal to 60.
+//     // - "CD" if marks is greater than 40 and less than or equal to 50.
+//     // - "DD" if marks is greater than 30 and less than or equal to 40.
+//     // - "FF" if marks is less than or equal to 30.
+
+//     // 🚨 Assign a value to "marks" between 0 and 100
+
+        let marks = 75;
+
+//     // 🚨 Format your if...else if...else conditional below
+
+        if (marks > 90) {
+        console.log("AA")
+        } else if (marks > 80) {
+        console.log("AB")
+//
+        } else if (marks > 70) {
+        console.log("BB")
+
+        }else if (marks > 60) {
+        console.log("BC")
+
+        }else if (marks > 50) {
+        console.log("CC")
+
+        }else if (marks > 40) {
+        console.log("CD")
+
+        }else if (marks > 30) {
+        console.log("DD")
+
+        }else {
+   console.log("FF")
+        }
